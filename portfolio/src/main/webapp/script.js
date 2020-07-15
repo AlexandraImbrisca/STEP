13fe1854a13b9c9194a11a4971787db1cce10d5c
AlexandraImbrisca <imbrisca.alexandra6@yahoo.com>
2020-07-15 09:49:57 +0000

    Fix coding style and revise functions for theme switch and gallery display

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,90 +1,102 @@
-var galleryOn = false;
-var gallerySizePercent = 75;
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-var darkModeOn = false;
-var darkBackgroundColor = "#13293D";
-var darkButtonBackgroundColor = "#2A628F";
+let darkModeOn = false;
+let galleryShown = false;
 
-var brightButtonBackgroundColor = "#3E92CC";
-
-var windowWidth = window.outerWidth;
-
-function computeWindowSize(windowWidth) {
-    var initialSize = gallerySizePercent;
-    if (windowWidth < 600) {
-        initialSize *= 4;
-    } else if (windowWidth < 800) {
-        initialSize *= 2;
+class Theme {
+  constructor(
+      textColor, 
+      backgroundColor,
+      themeButtonColor,
+      menuButtonTextColor,
+      menuButtonBackgroundColor) {
+        this.textColor = textColor; 
+        this.backgroundColor = backgroundColor;
+        this.themeButtonColor = themeButtonColor;
+        this.menuButtonTextColor = menuButtonTextColor;
+        this.menuButtonBackgroundColor = menuButtonBackgroundColor;
     }
-    return initialSize;
 }
 
-function recomputeWindowWidth() {
-    var newWindowWidth = window.outerWidth;
-    if (newWindowWidth == windowWidth) {
-        return;
-    }
-    var newGallerySize = computeWindowSize(newWindowWidth);
-    scaleColumns(newGallerySize);
+const DARK_THEME = new Theme('white', '#13293d', 'white', 'white', '#2a628f');
+const BRIGHT_THEME = new Theme('black', 'white', 'black', 'black', '#3e92cc');
+
+const GALLERY_SIZE_PERCENT = 75;
+
+function applyTheme(theme) {
+  var body = document.body;
+  var menuButtons = document.getElementsByClassName('menu-button');
+  var themeButton = document.getElementById('switch-theme-button');
+
+  body.style.color = theme.textColor;
+  body.style.backgroundColor = theme.backgroundColor;
+  themeButton.style.backgroundColor = theme.themeButtonColor;
+
+  for (var i = 0; i < menuButtons.length; i++) { 
+    menuButtons[i].style.color = theme.menuButtonTextColor;
+    menuButtons[i].style.backgroundColor = theme.menuButtonBackgroundColor;
+  }
 }
 
-function scaleColumns(gallerySize) {
-    var columns = document.getElementsByClassName("column");
-    var columnSize = gallerySize / columns.length + "%";
-        
-    for (var i = 0; i < columns.length; i++) {
-        columns[i].style.flex = columnSize;
-        columns[i].style.maxWidth = columnSize;
-    }
+function computeColumnSize(columns, gallerySize) {
+  var columnSize = GALLERY_SIZE_PERCENT / columns.length;
+  if (gallerySize < 600) {
+    columnSize *= 4;
+  } else if (gallerySize < 800) {
+    columnSize *= 2;
+  }
+  return columnSize + '%';
+}
+
+function displayColumns() {
+  var gallerySize = document.getElementById('gallery').offsetWidth;
+  var columns = document.getElementsByClassName('column');
+  var columnSize = computeColumnSize(columns, gallerySize);
+  
+  scaleColumns(columns, columnSize);
+}
+
+function scaleColumns(columns, columnSize) {
+  for (var i = 0; i < columns.length; i++) {
+    columns[i].style.flex = columnSize;
+    columns[i].style.maxWidth = columnSize;
+  }
 }
 
 function showGallery() {
-    var gallery = document.getElementById("gallery");
-    if (!galleryOn) {
-        galleryOn = true;
+  var gallery = document.getElementById('gallery');
+  if (galleryShown) {
+    galleryShown = false;
+    gallery.style.display = 'none';
+  } else {
+    galleryShown = true;
+    gallery.style.display = 'flex';
+    gallery.style.justifyContent = 'center';
 
-        gallery.style.display = "flex";
-        gallery.style.justifyContent = "center";
-
-        windowWidth = window.outerWidth;
-        var gallerySize = computeWindowSize(windowWidth);
-        scaleColumns(gallerySize);
-    } else {
-        galleryOn = false;
-        gallery.style.display = "none";
-    }
+    displayColumns();
+  }
 }
 
-function switchThemeFunction() {
-    var body = document.body;
-    var menuButtons = document.getElementsByClassName("menuButton");
-    var themeButton = document.getElementById("switchThemeButton");
-
-    if (!darkModeOn) {
-        darkModeOn = true;
-
-        body.style.color = "white";
-        body.style.backgroundColor = darkBackgroundColor;
-
-        themeButton.style.backgroundColor = "white";
-        
-        for (var i = 0; i < menuButtons.length; i++) { 
-            menuButtons[i].style.color = "white";
-            menuButtons[i].style.backgroundColor = darkButtonBackgroundColor;
-        }
-    } else {
-        darkModeOn = false;
-
-        body.style.color = "black";
-        body.style.backgroundColor = "white";
-
-        themeButton.style.backgroundColor = "black";
-
-        for (var i = 0; i < menuButtons.length; i++) {
-            menuButtons[i].style.color = "black";
-            menuButtons[i].style.backgroundColor = brightButtonBackgroundColor;
-        }
-    }
+function switchTheme() {
+  if (darkModeOn) {
+    darkModeOn = false;
+    applyTheme(BRIGHT_THEME);
+  } else {
+    darkModeOn = true;
+    applyTheme(DARK_THEME);
+  }
 }
 
-window.addEventListener("resize", recomputeWindowWidth);
+window.addEventListener('resize', displayColumns);
